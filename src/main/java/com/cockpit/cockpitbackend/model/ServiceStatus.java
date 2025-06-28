635914305035df0fc025f907ff77ed7339865d27
Ariane <ariane.de@outlook.com>
2025-06-28 15:04:08 -0300

    feature: Implementing package model and class ServiceStatus

---
+++ src/main/java/com/cockpit/cockpitbackend/model/ServiceStatus.java
@@ -0,0 +1,22 @@
+package com.cockpit.cockpitbackend.model;
+
+public class ServiceStatus {
+
+    private String name;
+    private String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public ServiceStatus(String name, String status){
+        this.name = name;
+        this.status = status;
+
+
+    }
+}
