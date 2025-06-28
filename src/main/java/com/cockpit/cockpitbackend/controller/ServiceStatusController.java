174f355e968e1dd019a4c89f916a2c33cd3877f3
Ariane <ariane.de@outlook.com>
2025-06-28 17:24:38 -0300

    refactor: fixing errors at pom file

--- src/main/java/com/cockpit/cockpitbackend/controller/ServiceStatusController.java
+++ src/main/java/com/cockpit/cockpitbackend/controller/ServiceStatusController.java
@@ -2,6 +2,7 @@ package com.cockpit.cockpitbackend.controller;
 
 import com.cockpit.cockpitbackend.model.ServiceStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,10 +12,10 @@ import java.util.List;
 public class ServiceStatusController {
 
     @GetMapping("/services")
-       public List<ServiceStatus> getServices() {
+       public List<ServiceStatus> getServices(@RequestParam(required = false)String name, @RequestParam(required = false)String status) {
         List<ServiceStatus>  services = new ArrayList<>();
         services.add(new ServiceStatus("Banco de dados", "UP"));
-        services.add(new ServiceStatus("API de usuários", "Down"));
+        services.add(new ServiceStatus("API de usuários", "DOWN"));
         services.add(new ServiceStatus("Integração de Erp", "UP"));
 
         return services;
