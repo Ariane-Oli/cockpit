b86cf5fe2c4c13f125b2bbec898e2b03e84f9e09
Ariane <ariane.de@outlook.com>
2025-07-05 17:17:01 -0300

    feature: Adding new filter

--- src/main/java/com/cockpit/cockpitbackend/controller/ServiceStatusController.java
+++ src/main/java/com/cockpit/cockpitbackend/controller/ServiceStatusController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class ServiceStatusController {
@@ -16,14 +17,25 @@ public class ServiceStatusController {
        public List<ServiceStatus> getServices(@RequestParam(required = false)String name, @RequestParam(required = false)String status) {
         List<ServiceStatus>  services = new ArrayList<>();
 
+        // 1- Adicionando os serviços
         services.add(new ServiceStatus("Banco de dados", "UP"));
         services.add(new ServiceStatus("API de usuários", "DOWN"));
         services.add(new ServiceStatus("Integração de Erp", "UP"));
 
-        services.stream().filter(service -> service.getStatus().equals("UP"))
-                .collect(Collectors.toList());
+        // 2- Cria stream a partir da lista já preenchida
+        Stream<ServiceStatus> stream = services.stream();
 
-        return services;
+       if(status != null) {
+           stream = stream.filter(service -> service.getStatus().equalsIgnoreCase(status));
+       }
+
+       if (name != null){
+           stream = stream.filter(service -> service.getName().toLowerCase().contains(name.toLowerCase()));
+       }
+
+       // mais filtros aqui
+       List<ServiceStatus> filtered = stream.collect(Collectors.toList());
+        return filtered;
 
 
 
