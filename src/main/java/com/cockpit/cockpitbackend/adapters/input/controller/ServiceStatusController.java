3bcf26df03cfdd6ff137676328f333b04c678926
Ariane <ariane.de@outlook.com>
2025-07-19 16:01:40 -0300

    fix(controller): adjust return type to use PaginatedResponse structure

--- src/main/java/com/cockpit/cockpitbackend/adapters/input/controller/ServiceStatusController.java
+++ src/main/java/com/cockpit/cockpitbackend/adapters/input/controller/ServiceStatusController.java
@@ -1,6 +1,7 @@
 package com.cockpit.cockpitbackend.adapters.input.controller;
 
 import com.cockpit.cockpitbackend.adapters.input.dto.PaginationRequest;
+import com.cockpit.cockpitbackend.adapters.output.dto.PaginatedResponse;
 import com.cockpit.cockpitbackend.domain.model.ServiceStatus;
 import com.cockpit.cockpitbackend.ports.input.ServiceStatusUseCasePort;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ public class ServiceStatusController {
         this.serviceStatusUseCasePort = serviceStatusUseCasePort;
     }
     @GetMapping
-       public List<ServiceStatus> getServices(@RequestParam(required = false)String name, @RequestParam(required = false)String status, PaginationRequest paginationRequest){
+       public PaginatedResponse<ServiceStatus> getServices(@RequestParam(required = false)String name, @RequestParam(required = false)String status, PaginationRequest paginationRequest){
 
         return serviceStatusUseCasePort.findServices(name,status, paginationRequest);
 
